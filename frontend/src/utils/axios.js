e5850f39b6089211e259fea34f0bd59c0f669fd6
rajpal-singh-code <rajpalku2022@gmail.com>
2026-01-30 00:14:11 +0530

    Revert "url changed"
    This reverts commit f285365535267ed0a2de1058a78539ac973a4e57.

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://skill-snap-ai.vercel.app",
+  baseURL: "https://skill-sprout-plum.vercel.app",
   withCredentials: true,
 });
 
